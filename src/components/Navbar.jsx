b3b6fc5be8194601d0c749008ceaa4ea18d3a98d
Eduardo <edu.rocha.aug@gmail.com>
2023-11-27 20:31:23 -0300

    search-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { CgProfile } from "react-icons/cg";
-import * as FaIcons from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
@@ -16,8 +16,13 @@ function Navbar() {
     <>
       <IconContext.Provider value={{ color: "undefined" }}>
         <div className="navbar">
+          <div className="flex items-center w-full justify-center">
+            <input id="search-input" className="h-12 w-[480px] rounded-full p-3 focus:outline-none focus:border-2 focus:border-amethyst-800 " type="text" />
+            
+            <label htmlFor="search-input"><FaSearch  className="text-white text-xl ml-4"/></label>
+          </div>
           <Link to="#" className="menu-bars">
-            <CgProfile  onClick={showSidebar} />
+            <CgProfile onClick={showSidebar} />
           </Link>
         </div>
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
